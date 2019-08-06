9c805d80b0bb7c5ea711be5a2b91229f87472380
siddhant <siddhant10shukla@gmail.com>
2019-08-06 20:05:41 +0530

    Fix create, add flash and error messages, create show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,19 +5,22 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    #render plain: params[:article].inspect   #getting whatever is send by the form in the new template
-    #cannot create article instance variable staright away from params, first we need to create a function to whitelist the
-    #information sent by the form
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
-    #redirecting to show path and show needs article id to display one specific articles
-    #that's why we pass the @article in the parameter
+    if @article.save #checking validations
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   private
     def article_params
       params.require(:article).permit(:title, :description)
     end
- 
+
 end

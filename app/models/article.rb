82f71a11f8ed203e11a3bac824171240bbe53a16
siddhant <siddhant10shukla@gmail.com>
2019-08-07 22:10:43 +0530

    add association between user and articles and debugger view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ApplicationRecord
+
+  belongs_to :user
   #title and description to be mandatory
   validates :title, presence: true , length: {minimum:3, maximum: 50}
   validates :description, presence: true, length: {minimum:10, maximum:300}
+  validates :user_id, presence: true
 end

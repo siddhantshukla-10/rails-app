a94ba1a1a06f8c7c1c0851ce033708ce399d9b17
siddhant <siddhant10shukla@gmail.com>
2019-08-08 10:43:41 +0530

    Complete Authentication System

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ApplicationRecord
 
   belongs_to :user
   #title and description to be mandatory
-  validates :title, presence: true , length: {minimum:3, maximum: 50}
-  validates :description, presence: true, length: {minimum:10, maximum:300}
+  validates :title, presence: true , length: {minimum:3, maximum: 200}
+  validates :description, presence: true, length: {minimum:10, maximum:3000}
   validates :user_id, presence: true
 end

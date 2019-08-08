141a8cde787b9f79c303af5e02dba52d96510d64
siddhant <siddhant10shukla@gmail.com>
2019-08-08 10:54:25 +0530

    Add signin upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save #checking validations
+      session[:user_id] = @user.id   #signing the user in
       flash[:success] = "Welcome to the alpha blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render "new"
     end

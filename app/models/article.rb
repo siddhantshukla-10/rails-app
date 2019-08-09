c7914a0827ec3d48c97a9df5b3e122ec0d52646b
siddhant <siddhant10shukla@gmail.com>
2019-08-09 10:18:04 +0530

    Add category and article associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord
 
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   #title and description to be mandatory
   validates :title, presence: true , length: {minimum:3, maximum: 200}
   validates :description, presence: true, length: {minimum:10, maximum:3000}

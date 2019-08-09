ecc808a24264ec2bc678e95613156b8e84fb64b3
siddhant <siddhant10shukla@gmail.com>
2019-08-09 07:25:42 +0530

    Fix tests and update navbar with category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,7 @@
 class CategoriesController < ApplicationController
 
+  before_action :require_admin, except: [:index, :show]
+
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
   end
@@ -26,5 +28,13 @@ class CategoriesController < ApplicationController
     def category_params
       params.require(:category).permit(:name)
     end
+
+    def require_admin
+      if !logged_in? || logged_in? and !current_user.try(:admin?)
+        flash[:danger] = "Only admins are allowed to do this"
+      redirect_to categories_path
+      end
+    end
+
 
 end
